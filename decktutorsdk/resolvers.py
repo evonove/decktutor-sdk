d1f38946046b703f9f13898c1e2eace82e6d76f2
Gioele Meoni <gioelemeoni@gmail.com>
2014-06-06 12:27:32 +0200

    Created base resolvers scheleton

--- decktutorsdk/resolvers.py
+++ decktutorsdk/resolvers.py
@@ -1,3 +1,6 @@
+import os
+from decktutorsdk import exceptions
+from decktutorsdk.api import Api
 
 
 class BaseResolver(object):
@@ -10,12 +13,66 @@ class BaseResolver(object):
             "You have to call am instance of an implemented Resolver"
         )
 
-class BaseResolver(object):
+
+class DefaultResolver(object):
     """
-    This is the very basic resolver
+    Default resolver
     """
 
     def resolve(self):
-        raise NotImplementedError(
-            "You have to call am instance of an implemented Resolver"
-        )
+        pass
+
+
+__auth_api__ = None
+__api__ = None
+
+
+def default_auth():
+    """
+    Returns the auth api object and if not present creates a new one
+    """
+    global __auth_api__
+    if __auth_api__ is None:
+        try:
+
+            username = os.environ["DECKTUTOR_USERNAME"]
+            password = os.environ["DECKTUTOR_PASSWORD"]
+        except KeyError:
+            raise exceptions.MissingConfig(
+                "DECKTUTOR_USERNAME and DECKTUTOR_PASSWORD not provided!"
+            )
+
+        __auth_api__ = Api(mode=os.environ.get("DECKTUTOR_MODE", "sandbox"),
+                           username=username, password=password, authenticate=True)
+    return __auth_api__
+
+
+def default():
+    """
+    Returns default api object and if not present creates a new one
+    """
+    global __api__
+    if __api__ is None:
+
+        __api__ = Api(mode=os.environ.get("DECKTUTOR_MODE", "sandbox"),
+                      authenticate=False)
+    return __auth_api__
+
+
+def set_auth_config(options=None, **config):
+    """
+    Create new api object with given configuration and authentication
+    """
+    global __auth_api__
+    __auth_api__ = Api(options or {}, **config)
+    return __auth_api__
+
+
+def set_default_config(options=None, **config):
+    """
+    Create new default api object with given configuration
+    """
+    global __auth_api__
+    __auth_api__ = Api(options or {}, **config)
+    return __auth_api__
+
