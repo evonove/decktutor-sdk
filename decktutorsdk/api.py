23c6104b12ed3bb60b5c89a39e68d969e844388a
Gioele Meoni <gioelemeoni@gmail.com>
2014-06-06 12:26:58 +0200

    Implemented the full stack of decktutor authentication in api object

--- decktutorsdk/api.py
+++ decktutorsdk/api.py
@@ -1,18 +1,17 @@
+import base64
+import hashlib
 import requests
 import json
 import logging
 import os
 import datetime
 import time
-from decktutorsdk.exceptions import MissingConfig
+from decktutorsdk.api_map import api_map
 
 import decktutorsdk.util as util
 
 from decktutorsdk import exceptions
 from decktutorsdk.version import __version__
-
-DECKTUTOR_V2 = "http://dev.decktutor.com/ws-2.0/app/v2"
-DECKTUTOR_SANDBOX_V2 = "http://dev.decktutor.com/ws-2.0/app/v2"
 
 
 class Api(object):
@@ -29,10 +28,12 @@ class Api(object):
         self.username = kwargs["username"]
         self.password = kwargs["password"]
         #end required
+
+        self.authenticate = kwargs.get("authenticate", False)
         self.mode = kwargs.get("mode", "sandbox")
         self.endpoint = kwargs.get("endpoint", self.default_endpoint())
-        self.token_endpoint = kwargs.get("token_endpoint", self.endpoint)
-        self.token_hash = None
+        self.token_endpoint = kwargs.get("token_endpoint", self.default_token_endpoint())
+        self.token = None
         self.token_request_at = None
         self.incremental = time.time()
         # setup SSL certificate verification if private certificate provided
@@ -40,63 +41,47 @@ class Api(object):
         if "cert" in ssl_options:
             os.environ["REQUESTS_CA_BUNDLE"] = ssl_options["cert"]
 
-        if kwargs.get("token"):
-            self.token_hash = {"access_token": kwargs["token"], "token_type": "Bearer"}
-
         self.options = kwargs
 
     def default_endpoint(self):
 
         if self.mode == "live":
-            return DECKTUTOR_V2
+            return api_map["api_root"]
         else:
-            return DECKTUTOR_SANDBOX_V2
+            return api_map["api_sandbox_root"]
+
+    def default_token_endpoint(self):
+
+        return api_map['api']['account']['login']['url']
 
     def basic_auth(self):
-        """Find basic auth, and returns base64 encoded
         """
-        credentials = "%s:%s" % (self.username, self.password)
-        return base64.b64encode(credentials.encode('utf-8')).decode('utf-8').replace("\n", "")
-
-    def get_token_hash(self, authorization_code=None, refresh_token=None):
-        """Generate new token by making a POST request
-
-            1. By using client credentials if validate_token_hash finds
-            token to be invalid. This is useful during web flow so that an already
-            authenticated user is not reprompted for login
-            2. Exchange authorization_code from mobile device for a long living
-            refresh token that can be used to charge user who has consented to future
-            payments
-            3. Exchange refresh_token for the user for a access_token of type Bearer
-            which can be passed in to charge user
-
+        Find basic auth, and returns base64 encoded
         """
-        path = "/account/login"
-        payload = "grant_type=client_credentials"
+        credentials = "{login:%s, password:%s}" % (self.username, self.password)
+        return base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
 
-        if authorization_code is not None:
-            payload = "grant_type=authorization_code&response_type=token&redirect_uri=urn:ietf:wg:oauth:2.0:oob&code=" + authorization_code
+    def get_token(self):
+        """
+        Generate new token by making a POST request
+        """
+        payload = self.basic_auth()
 
-        elif refresh_token is not None:
-            payload = "grant_type=refresh_token&refresh_token=" + refresh_token
-
+        self.validate_token_hash()
+        if self.token is not None:
+            return self.token
         else:
-            self.validate_token_hash()
-            if self.token_hash is not None:
-                return self.token_hash
-            else:
-                self.token_request_at = datetime.datetime.now()
+            self.token_request_at = datetime.datetime.now()
 
-        self.token_hash = self.http_call(
-            util.join_url(self.token_endpoint, path), "POST",
+        login = self.http_call(
+            self.token_endpoint, "POST",
             data=payload,
-            headers={
-                "Authorization": ("Basic %s" % self.basic_auth()),
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Accept": "application/json", "User-Agent": self.user_agent
-            })
+            headers=self.headers(authenticate=False))
 
-        return self.token_hash
+        self.token['auth_token'] = login['auth_token']
+        self.token['auth_token_expiration'] = login['auth_token_expiration']
+        self.token['auth_token_secret'] = login['auth_token_secret']
+        return self.token
 
     def sequence_number(self):
         """
@@ -107,31 +92,18 @@ class Api(object):
     def validate_token_hash(self):
         """Checks if token duration has expired and if so resets token
         """
-        if self.token_request_at and self.token_hash and self.token_hash.get("expires_in") is not None:
-            delta = datetime.datetime.now() - self.token_request_at
-            duration = (delta.microseconds + (delta.seconds + delta.days * 24 * 3600) * 10**6) / 10**6
-            if duration > self.token_hash.get("expires_in"):
-                self.token_hash = None
+        if self.token_request_at and self.token and self.token.get("auth_token_expiration") is not None:
+            if datetime.datetime.now() > util.parse_datetime(self.token.get("auth_token_expiration")):
+                self.token = None
 
-    def get_refresh_token(self, authorization_code=None):
-        if authorization_code is None:
-            raise exceptions.MissingConfig("Authorization code needed to get new refresh token. Refer to https://developer.paypal.com/docs/integration/mobile/make-future-payment/#get-an-auth-code")
-        return self.get_token_hash(authorization_code)["refresh_token"]
-
-    def request(self, url, method, body=None, headers=None, refresh_token=None):
+    def request(self, url, method, body=None, headers=None):
         """Make HTTP call, formats response and does error handling. Uses http_call method in API class.
-
         Usage::
-
-            >>> api.request("https://api.sandbox.paypal.com/v1/payments/payment?count=10", "GET", {})
-            >>> api.request("https://api.sandbox.paypal.com/v1/payments/payment", "POST", "{}", {} )
-
+            >>> api.request("https://api.decktutor.it/things", "GET", {})
+            >>> api.request("https://api.decktutor.it/other/things", "POST", "{}", {} )
         """
 
-        http_headers = util.merge_dict(self.headers(refresh_token=refresh_token), headers or {})
-
-        if http_headers.get('PayPal-Request-Id'):
-            logging.info('PayPal-Request-Id: %s' % (http_headers['PayPal-Request-Id']))
+        http_headers = util.merge_dict(self.headers(), headers or {})
 
         try:
             return self.http_call(url, method, data=json.dumps(body), headers=http_headers)
@@ -142,8 +114,8 @@ class Api(object):
 
         # Handle Expired token
         except exceptions.UnauthorizedAccess as error:
-            if(self.token_hash and self.username):
-                self.token_hash = None
+            if self.token and self.username and self.password:
+                self.token = None
                 return self.request(url, method, body, headers)
             else:
                 raise error
@@ -195,68 +167,29 @@ class Api(object):
         else:
             raise exceptions.ConnectionError(response, content, "Unknown response code: #{response.code}")
 
-    def headers(self, refresh_token=None):
+    def headers(self, authenticate=None):
 
-        token_hash = self.get_token_hash(refresh_token=refresh_token)
+        if authenticate is None:
+            authenticate = self.authenticate
 
-        return {
-            "x-dt-Auth-Token": ("%s" % token_hash['auth_token']),
-            "x-dt-Signature": ("%s" % self.sequence_number()),
-            "x-dt-Sequence": ("%s" % token_hash['signed_token']),
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "User-Agent": self.user_agent
-        }
+        if authenticate:
+            token = self.get_token()
+            sequence = self.sequence_number()
+            headers = {
+                "x-dt-Auth-Token": ("%s" % token['auth_token']),
+                "x-dt-Signature": (
+                    "%s" % hashlib.md5("%02d:%s" % (sequence, token['auth_token_secret'])).hexdigest()
+                ),
+                "x-dt-Sequence": ("%s" % sequence),
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "User-Agent": self.user_agent
+            }
+        else:
+            headers = {
 
-    def get(self, action, headers=None):
-
-        return self.request(util.join_url(self.endpoint, action), 'GET', headers=headers or {})
-
-    def post(self, action, params=None, headers=None, refresh_token=None):
-
-        return self.request(util.join_url(self.endpoint, action), 'POST', body=params or {},
-                            headers=headers or {}, refresh_token=refresh_token)
-
-    def put(self, action, params=None, headers=None, refresh_token=None):
-
-        return self.request(util.join_url(self.endpoint, action), 'PUT', body=params or {},
-                            headers=headers or {}, refresh_token=refresh_token)
-
-    def delete(self, action, headers=None):
-
-        return self.request(util.join_url(self.endpoint, action), 'DELETE', headers=headers or {})
-
-
-#the global decktutor api!
-__api__ = None
-
-
-def default():
-    """
-    Returns default api object and if not present creates a new one
-    """
-    global __api__
-    if __api__ is None:
-        try:
-
-            username = os.environ["DECKTUTOR_USERNAME"]
-            password = os.environ["DECKTUTOR_PASSWORD"]
-        except KeyError:
-            raise exceptions.MissingConfig(
-                "DECKTUTOR_USERNAME and DECKTUTOR_PASSWORD not provided!"
-            )
-
-        __api__ = Api(mode=os.environ.get("DECKTUTOR_MODE", "sandbox"),
-                      username=username, password=password)
-    return __api__
-
-
-def set_config(options=None, **config):
-    """
-    Create new default api object with given configuration
-    """
-    global __api__
-    __api__ = Api(options or {}, **config)
-    return __api__
-
-configure = set_config
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "User-Agent": self.user_agent
+            }
+        return headers
--- decktutorsdk/api_map.py
+++ decktutorsdk/api_map.py
@@ -11,6 +11,11 @@ _API_MAP = {
 
             'account': {
 
+                'login': {
+                    'url': '/account/login',
+                    'description': 'Create a logged in user session to authenticate future requests',
+                    'method': 'POST'
+                }
             },
             'cdb': {
 
@@ -24,67 +29,67 @@ _API_MAP = {
                     'url': '/insertions/create/{game}/{category}',
                     'description': 'Create a new insertion in a given category',
                     'methods': 'POST',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'info': {
                     'url': '/insertions/{code}/',
                     'description': 'Retrieve basic information about an insertion',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'page': {
                     'url': '/insertions/{code}/page',
                     'description': 'Retrieve all the information for an insertion',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'publish': {
                     'url': '/insertions/{code}/publish',
                     'description': 'Publish an unpublished insertion',
                     'methods': 'POST',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'publish_list': {
                     'url': '/insertions/{code}/publish',
                     'description': 'Publishes any number of insertions from the selling list',
                     'methods': 'POST',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'update': {
                     'url': '/insertions/{code}/field/{name}',
                     'description': 'Update one field of an existing insertion',
                     'methods': 'PUT',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'delete': {
                     'url': '/insertions/{code}/',
                     'description': 'Delete an existing insertion',
                     'methods': 'DELETE',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'messages': {
                     'url': '/insertions/{code}/publicMessages',
                     'description': 'Retrieve the list of public messages related to an insertion',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'create_message': {
                     'url': '/insertions/{code}/publicMessages',
                     'description': 'Post a public question or answer to an insertion',
                     'methods': 'POST',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'purchase': {
                     'url': '/insertions/{code}/purchase',
                     'description': 'Purchase an insertion',
                     'methods': 'POST',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'purchase_info': {
                     'url': '/insertions/purchases/',
                     'description': 'Retrieve the details of a previous purchase',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
             },
             'mytutor': {
@@ -93,13 +98,13 @@ _API_MAP = {
                     'url': '/mytutor/setSellerMotd',
                     'description': 'Find card versions related to a specific card name or set',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'payment_methods': {
                     'url': '/mytutor/paymentMethods/{payment_id}',
                     'description': 'Find card versions related to a specific card name or set',
                     'method': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
             },
             'search': {
@@ -108,49 +113,49 @@ _API_MAP = {
                     'url': '/search/card/name',
                     'description': 'Find card versions related to a specific card name or set',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'list_categories': {
                     'url': '/search/set/{game}/{code}/categories',
                     'description': 'List available categories for a particular expansion set',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'list_filters': {
                     'url': '/search/category/{code}/filters',
                     'description': 'List all possible filters for a category including possible values',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'card_version': {
                     'url': '/search/card/version',
                     'description': 'Find products in a specific category',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'product_list': {
                     'url': '/search/products/{code}',
                     'description': 'Find card official names matching a text query',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'serp': {
                     'url': '/search/serp',
                     'description': 'Main search endpoint',
                     'methods': 'POST',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'self_serp': {
                     'url': '/search/self/serp',
                     'description': 'Search through the insertions database for own insertions',
                     'methods': 'POST',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
                 'code': {
                     'url': '/search/insertion/code',
                     'description': 'Web service to autocomplete type-ahead',
                     'methods': 'GET',
-                    'resolver': "decktutorsdk.resolvers.AuthResolver",
+                    'resolver': 'decktutorsdk.resolvers.AuthResolver',
                 },
             },
             'sys': {
